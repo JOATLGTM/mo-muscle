cab1bd11492c72db8968ef8fc898c1cb798fa9b7
Chris Vo <cv@Chriss-MacBook-Pro.local>
2025-01-10 00:02:47 -0700

    added mobile response to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -83,7 +83,7 @@ export default function Home() {
 	return (
 		<div className="bg-black text-white">
 			{/* Hero Section */}
-			<section className="relative min-h-screen">
+			<section className="relative min-h-screen pt-16 md:pt-24 overflow-hidden">
 				<Image
 					src="/deadlift.jpg"
 					alt="Deadlift Exercise"
@@ -91,7 +91,7 @@ export default function Home() {
 					className="object-cover brightness-50"
 					priority
 				/>
-				<div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+				<div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
 					<p className="text-sm tracking-wider">
 						THE MIND IS PRIMARY
 					</p>
@@ -115,16 +115,16 @@ export default function Home() {
 			{/* Train With Gym Mo Section */}
 			<section
 				ref={trainWithRef}
-				className="py-24 text-center bg-white text-black"
+				className="py-12 md:py-24 text-center bg-white text-black"
 			>
 				<div className="container mx-auto px-4">
-					<p className="text-sm tracking-wider mb-8 text-[#0283C0]">
+					<p className="text-sm tracking-wider mb-4 md:mb-8 text-[#0283C0]">
 						FIND THE PROBLEM, FIX THE PROBLEM
 					</p>
-					<h2 className="text-4xl font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
+					<h2 className="text-3xl md:text-4xl font-bold mb-4 md:mb-8 bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
 						TRAIN WITH MO
 					</h2>
-					<p className="max-w-3xl mx-auto text-gray-600 mb-12">
+					<p className="max-w-3xl mx-auto text-gray-600 mb-8 md:mb-12 text-sm md:text-base">
 						At MoMuscle, we believe in a personalized approach to
 						fitness. Our trainers guide you through each step,
 						ensuring you achieve your physical goals effectively.
@@ -136,8 +136,11 @@ export default function Home() {
 			</section>
 
 			{/* Seminar Section */}
-			<section ref={seminarRef} className="py-24 bg-black text-white">
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-12 container mx-auto px-4 items-center">
+			<section
+				ref={seminarRef}
+				className="py-12 md:py-24 bg-black text-white"
+			>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-12 container mx-auto px-4 items-center">
 					<div className="relative aspect-square overflow-hidden rounded-lg shadow-2xl">
 						<Image
 							src="/IMG_8380.JPG"
@@ -147,13 +150,13 @@ export default function Home() {
 						/>
 					</div>
 					<div className="flex flex-col justify-center">
-						<p className="text-sm tracking-wider mb-4 text-[#0283C0]">
+						<p className="text-sm tracking-wider mb-2 md:mb-4 text-[#0283C0]">
 							LEARN WITH US
 						</p>
-						<h2 className="text-4xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
+						<h2 className="text-3xl md:text-4xl font-bold mb-4 md:mb-6 bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
 							MO MUSCLE TRAINING SERIES
 						</h2>
-						<p className="text-gray-300 mb-8">
+						<p className="text-gray-300 mb-6 md:mb-8 text-sm md:text-base">
 							Our trainers are dedicated professionals who
 							specialize in transforming your fitness journey.
 							With personalized training plans and unwavering
@@ -169,16 +172,16 @@ export default function Home() {
 			{/* Our Company Section */}
 			<section
 				ref={companyRef}
-				className="py-24 text-center bg-white text-black"
+				className="py-12 md:py-24 text-center bg-white text-black"
 			>
 				<div className="container mx-auto px-4">
-					<p className="text-sm tracking-wider mb-8 text-[#0283C0]">
+					<p className="text-sm tracking-wider mb-4 md:mb-8 text-[#0283C0]">
 						THE FIRST RULE OF ANY PHYSICAL TRAINING
 					</p>
-					<h2 className="text-4xl font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
+					<h2 className="text-3xl md:text-4xl font-bold mb-4 md:mb-8 bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
 						THE FORM
 					</h2>
-					<p className="max-w-3xl mx-auto text-gray-600 mb-12">
+					<p className="max-w-3xl mx-auto text-gray-600 mb-8 md:mb-12 text-sm md:text-base">
 						Mo Muscle is a collective of insanely dedicated and
 						hard-working people interested in self-improvement and
 						elite physical and mental performance. We don't like
@@ -193,10 +196,17 @@ export default function Home() {
 			<ScrollingText />
 
 			{/* Pricing Section */}
-			<section ref={pricingRef} className="py-24 bg-white text-black">
+			<section
+				ref={pricingRef}
+				className="py-12 md:py-24 bg-white text-black"
+			>
+				{" "}
 				<div className="container mx-auto px-4">
-					<div className="text-center mb-12">
-						<h2 className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
+					<div className="text-center mb-8 md:mb-12">
+						<p className="text-[#DB4D4D] text-sm tracking-wider mb-2 md:mb-4">
+							PRICING PLAN
+						</p>
+						<h2 className="text-3xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
 							Our Pricing Plan
 						</h2>
 					</div>
@@ -249,16 +259,16 @@ export default function Home() {
 			{/* Certified Instructors Section */}
 			<section
 				ref={instructorsRef}
-				className="py-24 text-center bg-black text-white"
+				className="py-12 md:py-24 text-center bg-black text-white"
 			>
 				<div className="container mx-auto px-4">
-					<p className="text-sm tracking-wider mb-4 text-[#0283C0]">
+					<p className="text-sm tracking-wider mb-2 md:mb-4 text-[#0283C0]">
 						FIND A COACH
 					</p>
-					<h2 className="text-4xl font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
+					<h2 className="text-3xl md:text-4xl font-bold mb-4 md:mb-8 bg-clip-text text-transparent bg-gradient-to-r from-[#0283C0] to-[#03a9f4] animate-text-shimmer">
 						CERTIFIED INSTRUCTORS
 					</h2>
-					<p className="max-w-3xl mx-auto text-gray-300 mb-12">
+					<p className="max-w-3xl mx-auto text-gray-300 mb-8 md:mb-12 text-sm md:text-base">
 						Our Certified instructors have passed through our
 						rigorous process of physical and mental testing to
 						represent Gym Jones wherever they coach. They embrace
@@ -269,7 +279,8 @@ export default function Home() {
 					</p>
 
 					<div className="relative">
-						<div className="flex overflow-x-auto gap-6 pb-6 justify-center">
+						<div className="flex flex-col md:flex-row overflow-x-auto gap-6 pb-6 justify-center">
+							{" "}
 							<InstructorCard
 								name="MO NAYAL"
 								location="COLUMBUS OHIO"
@@ -283,7 +294,8 @@ export default function Home() {
 						</div>
 					</div>
 					<Link href="/trainers">
-						<Button className="mt-12 bg-[#0283C0] hover:bg-[#026a9c] text-white transform hover:scale-105 transition-all duration-300">
+						<Button className="mt-8 md:mt-12 bg-[#0283C0] hover:bg-[#026a9c] text-white transform hover:scale-105 transition-all duration-300">
+							{" "}
 							SEE ALL INSTRUCTORS
 						</Button>
 					</Link>
@@ -295,7 +307,8 @@ export default function Home() {
 
 function InstructorCard({ name, location, image }) {
 	return (
-		<div className="min-w-[300px] group cursor-pointer transform hover:scale-105 transition-all duration-300">
+		<div className="min-w-[250px] md:min-w-[300px] group cursor-pointer transform hover:scale-105 transition-all duration-300">
+			{" "}
 			<div className="relative aspect-square mb-4 overflow-hidden rounded-lg">
 				<Image
 					src={image}
